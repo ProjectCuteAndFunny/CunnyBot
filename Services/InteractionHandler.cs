1d0b07366436ac52c5a8b3dbf231fbdf521d5290
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2022-09-06 16:56:48 +0300

    Update InteractionHandler.cs

--- Services/InteractionHandler.cs
+++ Services/InteractionHandler.cs
@@ -6,29 +6,30 @@ using Discord.WebSocket;
 namespace CunnyBot.Services;
 public class InteractionHandler
 {
-    private readonly DiscordSocketClient _client;
-    private readonly InteractionService _commands;
-    private readonly IServiceProvider _services;
+    private DiscordSocketClient Client { get; }
+    private InteractionService Commands { get; }
+    private IServiceProvider Services { get; }
 
     // Using constructor injection
     public InteractionHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
     {
-        _client = client;
-        _commands = commands;
-        _services = services;
+        Client = client;
+        Commands = commands;
+        Services = services;
     }
     public async Task InitializeAsync()
     {
         // Add the public modules that inherit InteractionModuleBase<T> to the InteractionService
-        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
 
         // Process the InteractionCreated payloads to execute Interactions commands
-        _client.InteractionCreated += HandleInteraction;
+        Client.InteractionCreated += HandleInteraction;
+        _= Client.SetGameAsync("Blue Archive", "https://www.twitch.tv/directory/game/Blue%20Archive", ActivityType.Competing);
 
         // Process the command execution results 
-        _commands.SlashCommandExecuted += SlashCommandExecuted;
-        _commands.ContextCommandExecuted += ContextCommandExecuted;
-        _commands.ComponentCommandExecuted += ComponentCommandExecuted;
+        Commands.SlashCommandExecuted += SlashCommandExecuted;
+        Commands.ContextCommandExecuted += ContextCommandExecuted;
+        Commands.ComponentCommandExecuted += ComponentCommandExecuted;
     }
     private static Task ComponentCommandExecuted(ComponentCommandInfo arg1, IInteractionContext arg2, IResult arg3) => Task.CompletedTask;
 
@@ -40,8 +41,8 @@ public class InteractionHandler
     {
         try {
             // Create an execution context that matches the generic type parameter of your InteractionModuleBase<T> modules
-            var ctx = new SocketInteractionContext(_client, arg);
-            await _commands.ExecuteCommandAsync(ctx, _services);
+            var ctx = new SocketInteractionContext(Client, arg);
+            await Commands.ExecuteCommandAsync(ctx, Services);
         } catch (Exception ex) {
             Console.WriteLine(ex);
 
